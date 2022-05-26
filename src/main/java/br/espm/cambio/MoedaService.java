3b440a118a97ee8f9b7e6c20af3be583471f76f9
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2022-05-26 11:56:21 -0300

    Update

--- src/main/java/br/espm/cambio/MoedaService.java
+++ src/main/java/br/espm/cambio/MoedaService.java
@@ -8,25 +8,40 @@ import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/* 
+ * Esse e o microservico de moeda
+ */
 @Component
 public class MoedaService {
-    
+
     @Autowired
     private MoedaRepository moedaRepository;
 
     public List<Moeda> listaAll() {
         return StreamSupport
-            //Transforma de iteravel para lista
-            .stream(moedaRepository.findAll().spliterator(), false)
-            .collect(Collectors.toList())
-            //Transforma de Model para Objeto
-            .stream().map(MoedaModel::to)
-            .collect(Collectors.toList());
-
+                // Transforma de iteravel para lista
+                .stream(moedaRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList())
+                // Transforma de Model para Objeto
+                .stream().map(MoedaModel::to)
+                .collect(Collectors.toList());
     }
 
     public Moeda create(Moeda vo) {
         vo.setId(UUID.randomUUID());
         return moedaRepository.save(new MoedaModel(vo)).to();
     }
+
+    public Moeda findBySimbolo(String simbolo) {
+        return moedaRepository.findBySimbolo(simbolo)
+                    .map(MoedaModel::to)
+                    .orElse(null);
+    }
+
+    public Moeda findBy(UUID id) {
+        return moedaRepository.findById(id.toString())
+                    .map(MoedaModel::to)
+                    .orElse(null);
+    }
+    
 }
